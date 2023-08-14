ca5fc4a304e99398afcc536534e5006bbf460c04
Robert Maina <robert-devs>
2023-08-14 12:58:36 +0300

    updates on upload file

--- lib/validations/account.schema.ts
+++ lib/validations/account.schema.ts
@@ -1,19 +1,17 @@
-import { z } from 'zod';
+import * as z from "zod";
 
-export const userValidationSchema = z.object({
-	profile_photo: z.string().nonempty({ message: "Email is required" }).email({ message: "Invalid email address" }),
-	name: z
-        .string()
-        .min(3, { message: "Minimum 3 characters." })
-        .max(30, { message: "Maximum 30 caracters." }),
-    username: z
-        .string()
-        .min(3, { message: "Minimum 3 characters." })
-        .max(30, { message: "Maximum 30 caracters." }),
-    bio: z
-        .string()
-        .min(3, { message: "Minimum 3 characters." })
-        .max(1000, { message: "Maximum 1000 caracters." }),
-}).required();
-
-export type LoginFormData = z.infer<typeof userValidationSchema>;
+export const UserValidation = z.object({
+  profile_photo: z.string().url().nonempty(),
+  name: z
+    .string()
+    .min(3, { message: "Minimum 3 characters." })
+    .max(30, { message: "Maximum 30 caracters." }),
+  username: z
+    .string()
+    .min(3, { message: "Minimum 3 characters." })
+    .max(30, { message: "Maximum 30 caracters." }),
+  bio: z
+    .string()
+    .min(3, { message: "Minimum 3 characters." })
+    .max(1000, { message: "Maximum 1000 caracters." }),
+});
